1e358ebfb24910977e75503b9e6fc1b6955d1202
sunny5130 <sunny51305527.com>
2026-04-03 16:30:35 +0530

    Abstraction and abstract classes

---
+++ TCS IPA/Java/oops/Abstract.java
@@ -0,0 +1,38 @@
+
+abstract class Car{
+    void start(){
+        System.out.println("Car is running");
+    }
+    abstract void acc();
+    abstract void brake();
+}
+class fuelCar extends Car{
+    //Writing @Override is optional
+    @Override void acc(){
+        System.out.println("Fuel car is running");
+    }
+    @Override void brake(){
+        System.out.println("Fuel car is going to Stop");
+    }
+}
+class oilCar extends Car{
+   void acc(){
+        System.out.println("Oil car is ready to running");
+    }
+    void brake(){
+        System.out.println("Oilcar is ready to stop");
+    }
+}
+public class Abstract{
+    public static void main(String[] args){
+        Car c=new fuelCar();
+        c.start();
+        c.acc();
+        c.brake();
+        
+        c=new oilCar();
+        c.start();
+        c.acc();
+        c.brake();
+        }
+}
